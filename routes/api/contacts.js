84c28352cdf770a54a3e7994da92ffaf7f7cd06c
Andrey <artist_hire@ukr.net>
2022-08-10 11:27:08 +0300

    docs: Add comments for routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,11 +3,17 @@ const contactApi = require('../../models/contacts');
 
 const router = express.Router();
 
+/**
+ * It`s the route that sends a response with a list of all contacts from the database
+ */
 router.get('/', async (req, res, next) => {
   const contacts = await contactApi.listContacts();
   res.json({status: 200, data: contacts});
 });
 
+/**
+ * It`s the route that finds a contact by Id in the database and sends a response with a found contact
+ */
 router.get('/:contactId', async (req, res, next) => {
   const {contactId} = req.params;
   const contact = await contactApi.getContactById(contactId);
@@ -20,6 +26,9 @@ router.get('/:contactId', async (req, res, next) => {
   res.json({status: 200, data: contact});
 });
 
+/**
+ * It`s the route that adds a new contact in the database and sends an added contact
+ */
 router.post('/', async (req, res, next) => {
   const {name, email, phone} = req.body;
 
@@ -32,6 +41,9 @@ router.post('/', async (req, res, next) => {
   res.status(201).json(newContact);
 });
 
+/**
+ * It`s the route that removes a contact by Id in the database and sends a removed contact
+ */
 router.delete('/:contactId', async (req, res, next) => {
   const {contactId} = req.params;
   const removedContact = await contactApi.removeContact(contactId);
@@ -44,6 +56,9 @@ router.delete('/:contactId', async (req, res, next) => {
   res.json({message: 'contact deleted'});
 });
 
+/**
+ * It`s the route that updates a contact by Id and new data in the database and sends an updated contact
+ */
 router.put('/:contactId', async (req, res, next) => {
   if (!req.body) {
     res.status(400).json({message: 'missing fields'});
